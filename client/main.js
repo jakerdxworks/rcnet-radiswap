21a9639bbc969f3c1674dd57c07b845d46c59fb1
jake-rdxworks <jakephucmai@gmail.com>
2023-05-02 16:20:35 -0400

    Clean up

--- client/main.js
+++ client/main.js
@@ -90,80 +90,13 @@ const streamApi = new StreamApi();
 
 let accountAddress // User account address
 let componentAddress = "component_tdx_c_1qdqsjrkdklx0a880vjf3q4n3cvf2x2nhgdjguk8cg42s34477x" 
-let resourceAddress // GUM resource address
 let packageAddress = "package_tdx_c_1qqfz89uh504w4ua0nvgx8ewel5jrp05glmdsggmyng5sldy63w"
-let tokenAAddress = "resource_tdx_c_1q8hgm0gdvrmffxytefwkm4ruarmjczp3syhuukc3n5tq32jlpq"
-let tokenBAddress = "resource_tdx_c_1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq40v2wv"
-let tokenAAmount = "10"
-let tokenBAmount = "10"
-let swapFee = "0.02"
+let tokenAAddress 
+let tokenBAddress 
+let swapFee
 let xrdAddress = "resource_tdx_c_1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq40v2wv"
-let poolunitsAddress = "resource_tdx_c_1q83fknuu5g60rmu95xchgwzn7yaexexq5kclkqeesk3s3v2a6d"
+let poolunitsAddress
 
-
-// document.getElementById('testTransaction').onclick = async function () {
-//   let test_manifest = new ManifestBuilder()
-//   .callMethod(
-//     accountAddress,
-//     "withdraw",
-//     [
-//       new ManifestAstValue.Address(
-//         xrdAddress
-//       ),
-//       new ManifestAstValue.Decimal(10),
-//     ]
-//   )
-//   .takeFromWorktop(
-//     xrdAddress,
-//     (builder, bucket) =>
-//       builder.callMethod(
-//         accountAddress,
-//         "deposit",
-//         [bucket]
-//       )
-//   )
-//   .build();
-
-//   console.log((
-//     await test_manifest.convert(
-//       InstructionList.Kind.String,
-//       NetworkId.RCnetV1
-//     )
-//   ).instructions.value)
-
-//   let converted_manifest = await test_manifest.convert(
-//     InstructionList.Kind.String,
-//     NetworkId.RCnetV1
-//   )
-
-//   let string_converted_manifest = converted_manifest.instructions.value;
-
-//   // let converted_manifest = await test_manifest.convert(
-//   //   InstructionList.Kind.String,
-//   //   NetworkId.RCnetV1
-//   // );
-
-//   console.log("Converted Manifest: ", string_converted_manifest)
-
-
-
-
-  // let formatted = test_convert.split('\n')
-  //   .map(line => '  ' + line.trim())
-  //   .join('\n');
-
-  // console.log("formatted: ", formatted)
-
-  // Send manifest to extension for signing
-//   const result = await rdt
-//     .sendTransaction({
-//       transactionManifest: string_converted_manifest,
-//       version: 1,
-//     })
-
-// if (result.isErr()) throw result.error
-
-// }
 
 document.getElementById('createToken').onclick = async function () {
   
@@ -209,38 +142,7 @@ if (result.isErr()) throw result.error
 
 console.log("Intantiate WalletSDK Result: ", result.value)
 
-// // ************ Fetch the transaction status from the Gateway API ************
-// let status = await transactionApi.transactionStatus({
-//   transactionStatusRequest: {
-//     intent_hash_hex: result.value.transactionIntentHash
-//   }
-// });
-// console.log('Instantiate TransactionApi transaction/status:', status)
-
-// // ************ Fetch component address from gateway api and set componentAddress variable **************
-// let commitReceipt = await transactionApi.transactionCommittedDetails({
-//   transactionCommittedDetailsRequest: {
-//     intent_hash_hex: result.value.transactionIntentHash
-//   }
-// })
-// console.log('Instantiate Committed Details Receipt', commitReceipt)
-// new_token = commitReceipt.details.referenced_global_entities[0];
-//   document.getElementById('newTokenAddress').innerText = new_token;
-
 }
-
-// document.getElementById('submit').onclick = async function () {
-//   // let fungibles: FungibleResourcesCollectionItem[] = []
-//   let accountState = await stateApi.stateEntityDetails({
-//     stateEntityDetailsRequest: {
-//       addresses: [accountAddress]
-//     }
-//   })
-
-//   // accountState.items[0].fungible_resources?.items.forEach(item => fungibles.push(item))
-//   // console.log(accountState.items[0].fungible_resources?.items[0].resource_address)
-//   console.log(accountState.items[0].fungible_resources?.items.forEach(item => fungibles.push(item)))
-// }
 
 
 window.onload = async function fetchData() {
@@ -253,8 +155,6 @@ window.onload = async function fetchData() {
   })
   
   accountState.items[0].fungible_resources?.items.forEach(item => fungibles.push(item))
-
-  // console.log(fungibles)
 
   const fungibles_string = [];
 
@@ -285,42 +185,20 @@ window.onload = async function fetchData() {
 
 }
 
-let tokenAMetadata = await stateApi.entityMetadataPage({
-  stateEntityMetadataPageRequest: {
-    address: tokenAAddress
-  }
-})
-
-
-
-console.log("Token A Metadata: ", tokenAMetadata)
-
-let metadata = tokenAMetadata.items[2].value.as_string;
-
-console.log("Metadata: ", metadata)
-
 // Retrieves TokenPair
-async function loadTokenPair() {
+async function loadTokenPair(tokenAAddress, tokenBAddress) {
 
-  let tokenAMetadata = await stateApi.entityMetadataPage({
-    stateEntityMetadataPageRequest: {
-      address: [tokenAAddress]
-    }
-  })
+  let tokenPair = [];
 
-  console.log("Token A Metadata: ", tokenAMetadata)
-
-  let token_pair = []
-
-  token_pair.push(tokenAAddress);
-  token_pair.push(tokenBAddress);
+  tokenPair.push(tokenAAddress);
+  tokenPair.push(tokenBAddress);
   
   var select = document.createElement("select");
 
   var swapDropDown = document.getElementById("swapDropDown");
   var exactSwapDropDown = document.getElementById("exactSwapDropDown");
   
-  for (const val of token_pair)
+  for (const val of tokenPair)
   {
       var option = document.createElement("option");
       option.value = val;
@@ -332,35 +210,34 @@ async function loadTokenPair() {
   
 }
 
-let token_pair = []
+async function loadPoolInformation() {
+  document.getElementById("tokenPair").innerText = tokenAAddress + "/" + tokenBAddress;
 
-token_pair.push(tokenAAddress);
-token_pair.push(tokenBAddress);
+  let tokenARequest = await stateApi.entityFungibleResourceVaultPage({
+    stateEntityFungibleResourceVaultsPageRequest: {
+      address: componentAddress,
+      resource_address: tokenAAddress,
+    }
+  })
 
-var select = document.createElement("select");
+  let tokenBRequest = await stateApi.entityFungibleResourceVaultPage({
+    stateEntityFungibleResourceVaultsPageRequest: {
+      address: componentAddress,
+      resource_address: tokenBAddress,
+    }
+  })
 
-var swapDropDown = document.getElementById("swapDropDown");
-var exactSwapDropDown = document.getElementById("exactSwapDropDown");
-
-for (const val of token_pair)
-{
-    var option = document.createElement("option");
-    option.value = val;
-    option.text = val.charAt(0) + val.slice(1);
-    select.appendChild(option);
-    swapDropDown.appendChild(option.cloneNode(true));
-    exactSwapDropDown.appendChild(option.cloneNode(true));
+  document.getElementById("liquidity").innerText = tokenARequest.items[0].amount + "/" + tokenBRequest.items[0].amount;
 }
-
 
 // ************ Instantiate component and fetch component and resource addresses *************
 document.getElementById('instantiateComponent').onclick = async function () {
   // let packageAddress = document.getElementById("packageAddress").value;
-  // let tokenAAddress = document.getElementById("selectTokenA").value;
-  // let tokenAAmount = document.getElementById("amountA").value;
-  // let tokenBAddress = document.getElementById("selectTokenB").value;
-  // let tokenBAmount = document.getElementById("amountB").value;
-  // let swapFee = document.getElementById("swapFee").value;
+  tokenAAddress = document.getElementById("selectTokenA").value;
+  let tokenAAmount = document.getElementById("amountA").value;
+  tokenBAddress = document.getElementById("selectTokenB").value;
+  let tokenBAmount = document.getElementById("amountB").value;
+  swapFee = document.getElementById("swapFee").value;
 
   let manifest = new ManifestBuilder()
     .callMethod(
@@ -368,7 +245,7 @@ document.getElementById('instantiateComponent').onclick = async function () {
       "withdraw",
       [
         new ManifestAstValue.Address(tokenAAddress),
-        new ManifestAstValue.Decimal(tokenBAmount),
+        new ManifestAstValue.Decimal(tokenAAmount),
       ]
     )    
     .callMethod(
@@ -392,7 +269,7 @@ document.getElementById('instantiateComponent').onclick = async function () {
           [
             tokenABucket,
             tokenBBucket,
-            new ManifestAstValue.Decimal("0.02")
+            new ManifestAstValue.Decimal(swapFee)
           ]
         )
       )
@@ -430,35 +307,34 @@ document.getElementById('instantiateComponent').onclick = async function () {
 
   if (result.isErr()) throw result.error
 
-  // console.log("Intantiate WalletSDK Result: ", result.value)
+  console.log("Intantiate WalletSDK Result: ", result.value)
 
-  // // ************ Fetch the transaction status from the Gateway API ************
-  // let status = await transactionApi.transactionStatus({
-  //   transactionStatusRequest: {
-  //     intent_hash_hex: result.value.transactionIntentHash
-  //   }
-  // });
-  // console.log('Instantiate TransactionApi transaction/status:', status)
+  // ************ Fetch the transaction status from the Gateway API ************
+  let status = await transactionApi.transactionStatus({
+    transactionStatusRequest: {
+      intent_hash_hex: result.value.transactionIntentHash
+    }
+  });
+  console.log('Instantiate TransactionApi transaction/status:', status)
 
-  // // ************ Fetch component address from gateway api and set componentAddress variable **************
-  // let commitReceipt = await transactionApi.transactionCommittedDetails({
-  //   transactionCommittedDetailsRequest: {
-  //     intent_hash_hex: result.value.transactionIntentHash
-  //   }
-  // })
-  // console.log('Instantiate Committed Details Receipt', commitReceipt)
+  // ************ Fetch component address from gateway api and set componentAddress variable **************
+  let commitReceipt = await transactionApi.transactionCommittedDetails({
+    transactionCommittedDetailsRequest: {
+      intent_hash_hex: result.value.transactionIntentHash
+    }
+  })
+  console.log('Instantiate Committed Details Receipt', commitReceipt)
 
-  // // ****** Set componentAddress variable with gateway api commitReciept payload ******
-  // // componentAddress = commitReceipt.details.referenced_global_entities[0]
-  // document.getElementById('componentAddress').innerText = componentAddress;
-  // // ****** Set resourceAddress variable with gateway api commitReciept payload ******
-  // poolunitsAddress = commitReceipt.details.referenced_global_entities[1]
-  // document.getElementById('poolunitsAddress').innerText = poolunitsAddress;
+  // ****** Set componentAddress variable with gateway api commitReciept payload ******
+  componentAddress = commitReceipt.details.referenced_global_entities[0]
+  document.getElementById('componentAddress').innerText = componentAddress;
+  // ****** Set resourceAddress variable with gateway api commitReciept payload ******
+  poolunitsAddress = commitReceipt.details.referenced_global_entities[1]
+  document.getElementById('poolunitsAddress').innerText = poolunitsAddress;
 
-  loadTokenPair();
+  loadTokenPair(tokenAAddress, tokenBAddress);
+  loadPoolInformation();
 }
-
-
 
 document.getElementById('swapToken').onclick = async function () {
   let inputToken = document.getElementById("swapDropDown").value;
@@ -514,14 +390,15 @@ document.getElementById('swapToken').onclick = async function () {
   if (result.isErr()) throw result.error
 
   console.log("Intantiate WalletSDK Result: ", result.value)
-  
+
+  loadPoolInformation();
 }
 
 document.getElementById('getAmount').onclick = async function () {
   let requestedToken = document.getElementById("exactSwapDropDown").value;
   let requestedAmount = document.getElementById("requestedAmount").value;
 
-  console.log(requestedAmount)
+  console.log(componentAddress)
 
     // Sorting logic
     let inputTokenAddress, outputTokenAddress;
@@ -532,6 +409,8 @@ document.getElementById('getAmount').onclick = async function () {
       inputTokenAddress = tokenAAddress;
       outputTokenAddress = tokenBAddress; 
     };
+
+    console.log(inputTokenAddress)
 
   // Making request to gateway
   let inputTokenRequest = await stateApi.entityFungibleResourceVaultPage(
@@ -554,18 +433,11 @@ document.getElementById('getAmount').onclick = async function () {
 
   console.log("Token B Request: ", outputTokenRequest);
 
-  // Retrieiving pool liquidity
-  // let token_a_amount = tokenArequest.items[0].amount;
-  // let token_b_amount = outputTokenRequest.items[0].amount;
 
   let x = inputTokenRequest.items[0].amount;
   let y = outputTokenRequest.items[0].amount;
-  // let x = self.vaults[&self.other_resource_address(output_resource_address)].amount();
-  // let y = self.vaults[&output_resource_address].amount();
   let dy = requestedAmount;
-  
   let r = (1 - swapFee) / 1;
-
   let dx = (dy * x) / (r * (y - dy));
 
   document.getElementById('requiredResource').innerText = inputTokenAddress
@@ -573,58 +445,35 @@ document.getElementById('getAmount').onclick = async function () {
 }
 
 document.getElementById('exactSwapToken').onclick = async function () {
-  let requiredResource = document.getElementById('requiredResource');
-  let requiredAmount = document.getElementById("requiredAmount").innerHTML;
-  let requestedResource = document.getElementById("exactSwapDropDown").value;
-  let requestedAmount = document.getElementById("requestedAmount").value;
-
-  console.log(requestedResource)
-  console.log(requestedAmount)
-  
-
-  // let requestedAmountNum = parseInt(requestedAmount);
-
-  // Sorting logic
-  let inputTokenAddress, outputTokenAddress;
-  if (requiredResource === tokenAAddress ) {
-    inputTokenAddress = tokenBAddress; 
-    outputTokenAddress = tokenAAddress;
-  } else {
-    inputTokenAddress = tokenAAddress;
-    outputTokenAddress = tokenBAddress; 
-  };
+  let requiredResource = document.getElementById('requiredResource').innerText;
+  let requiredAmount = document.getElementById("requiredAmount").innerHTML;  
 
   let manifest = new ManifestBuilder()
-  .callMethod(
-    accountAddress,
-    "withdraw",
-    [
-      new ManifestAstValue.Address(inputTokenAddress),
-      new ManifestAstValue.Decimal(requiredAmount),
-    ]
-  )
-  .takeFromWorktop(
-    inputTokenAddress,
-    (builder, inputBucket) => 
-    builder.callMethod(
-      componentAddress,
-      "swap",
-      [inputBucket]      
+    .callMethod(
+      accountAddress,
+      "withdraw",
+      [
+        new ManifestAstValue.Address(requiredResource),
+        new ManifestAstValue.Decimal(requiredAmount),
+      ]
     )
-  )
-  // bug
-  .assertWorktopContainsByAmount(
-    new ManifestAstValue.Address(requestedResource),
-    new ManifestAstValue.Decimal(requestedAmount)
-  )
-  .callMethod(
-    accountAddress,
-    "deposit_batch", 
-    [
-    ManifestAstValue.Expression.entireWorktop()
-    ]
-  )
-  .build();
+    .takeFromWorktop(
+      requiredResource,
+      (builder, requiredBucket) => 
+      builder.callMethod(
+        componentAddress,
+        "swap",
+        [requiredBucket]      
+      )
+    )
+    .callMethod(
+      accountAddress,
+      "deposit_batch", 
+      [
+      ManifestAstValue.Expression.entireWorktop()
+      ]
+    )
+    .build();
 
   console.log(manifest)
 
@@ -646,76 +495,26 @@ document.getElementById('exactSwapToken').onclick = async function () {
 
   if (result.isErr()) throw result.error
 
-  console.log("Intantiate WalletSDK Result: ", result.value)
-  
-}
+  console.log("Exact Swap sendTransaction Result: ", result)
 
-
-
-
-document.getElementById('getLiquidity').onclick = async function () {
-//   let manifest = new ManifestBuilder()
-//     .callMethod(
-//       componentAddress,
-//       "get_liquidity"
-//     )
-//     .build()
-//     .toString();
-
-//     const result = await rdt.sendTransaction({
-//       transactionManifest: manifest,
-//       version: 1,
-//     })
-
-// if (result.isErr()) throw result.error
-
-let tokenArequest = await stateApi.entityFungibleResourceVaultPage(
-  {
-    stateEntityFungibleResourceVaultsPageRequest: {
-      address: componentAddress,
-      resource_address: tokenAAddress,
+  // Fetch the transaction status from the Gateway SDK
+  let status = await transactionApi.transactionStatus({
+    transactionStatusRequest: {
+      intent_hash_hex: result.value.transactionIntentHash
     }
   });
+  console.log('Exact Swap TransactionAPI transaction/status: ', status)
 
-  console.log("Token A Request: ", tokenArequest)
+  // fetch commit reciept from gateway api 
+  let commitReceipt = await transactionApi.transactionCommittedDetails({
+    transactionCommittedDetailsRequest: {
+      intent_hash_hex: result.value.transactionIntentHash
+    }
+  })
+  console.log('Exact Swap Committed Details Receipt', commitReceipt)
 
-  let outputTokenRequest = await stateApi.entityFungibleResourceVaultPage(
-    {
-      stateEntityFungibleResourceVaultsPageRequest: {
-        address: componentAddress,
-        resource_address: tokenBAddress,
-      }
-    });
+  // Show the receipt on the DOM
+  document.getElementById('exactReceipt').innerText = JSON.stringify(commitReceipt.details.receipt, null, 2);
 
-  
-  console.log("Token B Request: ", outputTokenRequest)
-
-  let token_a_amount = tokenArequest.items[0].amount;
-  console.log("Token A Request: ", token_a_amount)
-  let token_b_amount = tokenBrequest.items[0].amount;
-
-  document.getElementById('tokenALiquidity').innerText = token_a_amount;
-  document.getElementById('tokenBLiquidity').innerText = token_b_amount;
-
-
+  loadPoolInformation();
 }
-
-
-
-
-
-// document.getElementById('getLiquidity').onclick = async function () {
-
-// }
-
-// document.getElementById('getLiquidity').onclick = async function () {
-
-// }
-
-// document.getElementById('getLiquidity').onclick = async function () {
-
-// }
-
-// document.getElementById('getLiquidity').onclick = async function () {
-
-// }
